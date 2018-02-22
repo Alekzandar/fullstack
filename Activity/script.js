783f3060672dfcb519c4a0127936e2089d9b1af9
Aleksandar Antonov <ala021@bucknell.edu>
2018-02-22 18:52:50 -0500

    Correcting errors on first iteration

---
+++ Activity/script.js
@@ -0,0 +1,11 @@
+
+
+
+//1
+var string = "Hello";
+// string += " World"; // uncomment to test
+string = string + " World";
+console.log(string + "!");
+// This example will print out 'Hello World World!' on the console. However, 
+// in the console I got "Hello World" without the section to be uncommented and
+// got the expected with that line uncommented.
